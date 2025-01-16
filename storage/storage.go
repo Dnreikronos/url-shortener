9da1e550d71aea2a008e8c58e345e9d90fa5421e
Dnreikronos <dnreikronos2@gmail.com>
2025-01-16 19:44:41 -0300

    feature: add packages in the import

--- storage/storage.go
+++ storage/storage.go
@@ -1,5 +1,13 @@
 package storage
 
+import (
+	"log"
+	"sync"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 type URLMapping struct {
 	id          uint   `gorm:"primaryKey"`
 	ShortKey    string `gorm:"uniqueIndex"`
