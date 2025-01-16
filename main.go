c9242dcbc1dca70357991d2ff80f7bcca8a312fe
Dnreikronos <dnreikronos2@gmail.com>
2025-01-15 21:38:24 -0300

    feat: create two variables with flag

--- main.go
+++ main.go
@@ -2,3 +2,7 @@ var (
 	urlStore = make(map[string]string)
 	mutex    = &sync.Mutex{}
 )
+	shorten := flag.String("Shorten", "", "Url to shorten")
+	expand := flag.String("Expand", "", "Shortened URL to Expand")
+
+	flag.Parse()
